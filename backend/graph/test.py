4bfa3b7eb4a45582790e93e8c521445f9197092b
Alejandro Afonso <alejandro.afonso@in2ai.com>
2026-01-29 17:00:54 +0000

    updated graph rag test

--- backend/graph/test.py
+++ backend/graph/test.py
@@ -2,6 +2,9 @@ from agent import graph
 from langgraph.graph import MessagesState
 
 if __name__ == "__main__":
+    config = {"configurable": {"thread_id": "1"}}
     query = MessagesState(messages=[("human", "Hello my friend")])  # , perform the test
-    response = graph.invoke(query)
+
+    response = graph.invoke({"messages": query}, config)
+
     print(response["messages"][-1].content)
