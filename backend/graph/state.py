16821f4c853df70f7eb530d3db59adbf451bd46f
Alejandro Afonso <alejandro.afonso@in2ai.com>
2026-01-29 18:04:20 +0000

    added reminder to change to pydantic

--- backend/graph/state.py
+++ backend/graph/state.py
@@ -1,6 +1,14 @@
 from langgraph.graph import MessagesState
 
+# from pydantic import BaseModel, field_validator, ValidationError
+
+# class PydanticState(BaseModel):
+#    name: str
+#    mood: str # "happy" or "sad"
+
+
 # TODO: think about needed information at each point of the graph
+# TODO: migration to PyDantic?
 
 
 class State(MessagesState):
