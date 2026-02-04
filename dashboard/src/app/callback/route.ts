a421f9e728fc7becc8eb2f2a98ca7f2e3278d519
mjmmattoni98 <javier.magni@gmail.com>
2026-02-04 14:14:02 +0100

    Update import as type import

--- dashboard/src/app/callback/route.ts
+++ dashboard/src/app/callback/route.ts
@@ -1,7 +1,7 @@
 import { logtoConfig } from "@/lib/logto";
 import { handleSignIn } from "@logto/next/server-actions";
 import { redirect } from "next/navigation";
-import { NextRequest } from "next/server";
+import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
