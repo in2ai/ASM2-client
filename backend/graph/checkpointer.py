d895a6986d2577c65a3a5297073f62cdf2e117f1
Alejandro Afonso <alejandro.afonso@in2ai.com>
2026-02-09 12:37:50 +0000

    kept persistence inmemory

--- backend/graph/checkpointer.py
+++ backend/graph/checkpointer.py
@@ -1,17 +1,21 @@
-from langgraph.checkpoint.postgres import PostgresSaver
+# from langgraph.checkpoint.postgres import PostgresSaver
+from langgraph.checkpoint.memory import InMemorySaver
 from psycopg import Connection
 
 # TODO: clarify connection to postgresSQL DB
 # # ref: https://medium.com/@dmitri.mahayana/chain-everything-with-langgraph-9a0f35b2d7a2
-DB_URI = "postgresql://langgraph:langgraph@localhost:5432/langgraph"
+# DB_URI = "postgresql://langgraph:langgraph@localhost:5432/langgraph"
 # DB_URI = os.getenv("POSTGRES_URI")
 
 # memory_db_uri = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=disable"
-memory_connection_kwargs = {
-    "autocommit": True,
-    "prepare_threshold": 0,
-}
-conn = Connection.connect(DB_URI, **memory_connection_kwargs)
+# memory_connection_kwargs = {
+#     "autocommit": True,
+#     "prepare_threshold": 0,
+# }
+# conn = Connection.connect(DB_URI, **memory_connection_kwargs)
 
-checkpointer = PostgresSaver(conn)
-checkpointer.setup()
+# checkpointer = PostgresSaver(conn)
+# checkpointer.setup()
+#
+
+checkpointer = InMemorySaver()
