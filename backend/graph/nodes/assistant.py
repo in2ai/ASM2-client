c4c53325ace6cbd3dcf232c5bde6bdda24380a7c
Alejandro Afonso <alejandro.afonso@in2ai.com>
2026-02-09 13:34:15 +0000

    added assistant system message consistency independent of summary

--- backend/graph/nodes/assistant.py
+++ backend/graph/nodes/assistant.py
@@ -1,17 +1,15 @@
 from langchain_core.messages import SystemMessage
 from model import llm_with_tools
 from state import State
+from src.utils.rag import get_rag_system_prompt
 
 
-# # Define the logic to call the model
 def call_model(state: State):
-    summary = state.summary
+    system_prompt = get_rag_system_prompt(state.detected_lang)
 
-    if summary:
-        system_message = f"Summary of conversation earlier: {summary}"
-        messages = [SystemMessage(content=system_message)] + state.messages
-    else:
-        messages = state.messages
+    if state.summary:
+        system_prompt += f"\n\nSummary of conversation earlier: {state.summary}"
 
+    messages = [SystemMessage(content=system_prompt)] + state.messages
     response = llm_with_tools.invoke(messages)
     return {"messages": response}
