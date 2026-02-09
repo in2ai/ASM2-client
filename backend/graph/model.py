51dffb40f3176d49689646311fe50caddde47ff0
Alejandro Afonso <alejandro.afonso@in2ai.com>
2026-02-09 13:27:12 +0000

    model.py becomes source of truth

--- backend/graph/model.py
+++ backend/graph/model.py
@@ -3,11 +3,12 @@ from pathlib import Path
 from dotenv.main import load_dotenv
 from langchain_openai.chat_models.base import ChatOpenAI
 from tools.test_tool import test_tool
+from tools.vectordb_search import vectordb_search
 
 load_dotenv(Path(__file__).resolve().parents[2] / ".env")
 
 # ChatOpenAI reads api_key from .env automatically
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
-tool_list = [test_tool]
+tool_list = [test_tool, vectordb_search]
 llm_with_tools = llm.bind_tools(tool_list, parallel_tool_calls=False)
