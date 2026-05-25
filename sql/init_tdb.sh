ec9f910595e35a5b05a4d8f35bc48a9b217fe0a9
Alejandro Afonso <alejandro.afonso@in2ai.com>
2026-05-25 10:52:57 +0100

    finish .env setup of tsdb init_tdb.sh

--- sql/init_tdb.sh
+++ sql/init_tdb.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -eu
 
-: "${PG_HOST:=PG}"
-: "${PG_PORT:=8812}"
-: "${PG_USER:=admin}"
-: "${PG_PASSWORD:=quest}"
-: "${PG_DB:=qdb}"
+: "${PG_HOST:=timescaledb}"
+: "${PG_PORT:=5432}"
+: "${PG_USER:=postgres}"
+: "${PG_PASSWORD:=}"
+: "${PG_DB:=tsdb}"
 
 echo "Waiting for PG PGWire on $PG_HOST:$PG_PORT ..."
 
@@ -14,10 +14,10 @@ until pg_isready -h "$PG_HOST" -p "$PG_PORT" -U "$PG_USER" >/dev/null 2>&1; do
   sleep 5
 done
 
-echo "TimescaleDB is reachable. Executing init.sql ..."
+echo "TimescaleDB is reachable. Executing init_tdb.sql ..."
 
 export PGPASSWORD="$PG_PASSWORD"
 
-psql -h "$PG_HOST" -p "$PG_PORT" -U "$PG_USER" -d "$PG_DB" -f init.sql
+psql -h "$PG_HOST" -p "$PG_PORT" -U "$PG_USER" -d "$PG_DB" -f init_tdb.sql
 
 echo "SQL applied successfully. Exiting."
